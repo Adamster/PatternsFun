08e048a635f6a499eca74c012098ffe47274d9a3
Adamster Domi <adamsterdomi@gmail.com>
2015-08-21 17:55:30 +0300

    rebuild

--- Infrastrucuture/IoC/ServiceLocator.cs
+++ Infrastrucuture/IoC/ServiceLocator.cs
@@ -1,12 +1,13 @@
-﻿// PatternsFun by Serghei Adam 
-// Created 29 07 2015 
-// Edited 03 08 2015
+﻿// File: ServiceLocator.cs in
+// PatternsFun by Serghei Adam 
+// Created 21 08 2015 
+// Edited 21 08 2015
 
 using ActionImplementation;
 using InterfacesActions;
 using Ninject;
-using Repository.Interfaces;
 using Repository;
+using Repository.Interfaces;
 
 namespace Infrastrucuture.IoC
 {
